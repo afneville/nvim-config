d39683c949f0b305f5ef58eb929712549c49ba41
Alexander Neville <git@afneville.com>
2021-06-19 23:02:03 +0100

    recovered lua utils (replace functions from lunarvim)

--- lua/lua_utils/init.lua
+++ lua/lua_utils/init.lua
@@ -1,5 +1,16 @@
-local utils = {}
+local scopes = {o = vim.o, b = vim.bo, w = vim.wo}
+local utils = { }
 
+function utils.opt(scope, key, value)
+    scopes[scope][key] = value
+    if scope ~= 'o' then scopes['o'][key] = value end
+end
+
+function utils.map(mode, lhs, rhs, opts)
+  local options = {noremap = true}
+  if opts then options = vim.tbl_extend('force', options, opts) end
+  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
 function utils.define_augroups(definitions) -- {{{1
     -- Create autocommand groups based on the passed definitions
     --
