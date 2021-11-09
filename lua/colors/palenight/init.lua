d88fc1385a3e3d6e0a7f7b83116ca4e0cbd70f18
Alexander Neville <alexander.neville@icloud.com>
2021-11-09 17:03:31 +0000

    2 new colour schemes

---
+++ lua/colors/palenight/init.lua
@@ -0,0 +1,31 @@
+
+vim.api.nvim_command "hi clear"
+if vim.fn.exists "syntax_on" then
+  vim.api.nvim_command "syntax reset"
+end
+vim.o.background = "dark"
+vim.o.termguicolors = true
+vim.g.colors_name = "nord"
+
+local util = require "colors.util"
+Config = require "colors.config"
+C = require "colors.palenight.palette"
+local highlights = require "colors.highlights"
+local Treesitter = require "colors.Treesitter"
+local markdown = require "colors.markdown"
+local Whichkey = require "colors.Whichkey"
+local Git = require "colors.Git"
+local LSP = require "colors.LSP"
+
+local skeletons = {
+  highlights,
+  Treesitter,
+  markdown,
+  Whichkey,
+  Git,
+  LSP,
+}
+
+for _, skeleton in ipairs(skeletons) do
+  util.initialise(skeleton)
+end
