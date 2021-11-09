a03249596e64c03f8a961532e0285261be6f6a80
Alexander Neville <alexander.neville@icloud.com>
2021-11-09 17:06:28 +0000

    general settings changes

--- lua/editor_settings.lua
+++ lua/editor_settings.lua
@@ -1,4 +1,3 @@
-
 vim.o.hidden = true
 vim.o.title = true
 TERMINAL = vim.fn.expand('$TERMINAL')
@@ -16,7 +15,7 @@ vim.wo.number = O.number
 vim.wo.relativenumber = O.relative_number
 vim.wo.cursorcolumn = false
 vim.wo.cursorline = false
-vim.o.showtabline = 2
+vim.o.showtabline = 0
 vim.o.showmode = false
 vim.o.backup = false
 vim.o.writebackup = false
@@ -24,21 +23,25 @@ vim.wo.signcolumn = "yes"
 vim.o.updatetime = 300
 vim.o.timeoutlen = O.timeoutlen
 vim.o.clipboard = "unnamedplus"
-vim.g.nvim_tree_disable_netrw = O.nvim_tree_disable_netrw
-vim.g.loaded_netrwPlugin = 1
+-- vim.g.loaded_netrwPlugin = 1
 vim.o.sidescrolloff = 8
 vim.o.wrap = false
 vim.o.smartcase = true
 
-vim.cmd('filetype plugin on')
 vim.cmd('let g:nvim_termcolors=256')
-vim.cmd('colorscheme ' .. O.colorscheme)
+vim.cmd('filetype plugin on')
+vim.cmd('syntax on')
+vim.cmd('colorscheme onedark')
 vim.cmd('set ts=4')
 vim.cmd('set sw=4')
 vim.cmd('set expandtab')
 vim.cmd('set whichwrap=<,>,[,]') -- move to next line with theses keys
-vim.cmd('syntax on')
 vim.cmd('set iskeyword+=-')
 vim.cmd('set shortmess+=c')
 vim.cmd('set inccommand=split')
 vim.cmd('let &titleold="'..TERMINAL..'"')
+
+
+vim.cmd("let g:fzf_preview_window = ['up:40%:hidden', 'ctrl-/']")
+-- vim.cmd("let g:fzf_prefer_tmux = 1")
+vim.cmd("let g:fzf_layout = { 'right': '~40%' }")
--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -20,17 +20,10 @@ vim.api.nvim_set_keymap('i', 'kj', '<ESC>', {noremap = true, silent = true})
 vim.api.nvim_set_keymap('i', 'jj', '<ESC>', {noremap = true, silent = true})
 
 -- use tab key to switch buffer in normal mode (using bar-bar instead)
--- vim.api.nvim_set_keymap('n', '<TAB>', ':bnext<CR>', {noremap = true, silent = true})
--- vim.api.nvim_set_keymap('n', '<S-TAB>', ':bprevious<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<TAB>', ':bnext<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<S-TAB>', ':bprevious<CR>', {noremap = true, silent = true})
 vim.api.nvim_set_keymap("n", "<S-l>", ":BufferNext<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<S-h>", ":BufferPrevious<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<TAB>', ':BufferNext<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<S-TAB>', ':BufferPrevious<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', 'gt', ':BufferPick<CR>', {})
-vim.api.nvim_set_keymap("n", "<leader>c", ":BufferClose<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<leader>C", ":BufferCloseAllButCurrent<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<leader>p", ":BufferPin<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<leader>P", ":BufferCloseAllButPinned<CR>", {noremap = true, silent = true})
 
 -- emacs style C-j/k scrolling
 vim.cmd('inoremap <expr> <c-j> (\"\\<C-n>\")')
@@ -46,28 +39,17 @@ utils.map('', '<leader>wh', '<C-w>h')
 utils.map('', '<leader>wj', '<C-w>j')
 utils.map('', '<leader>wk', '<C-w>k')
 utils.map('', '<leader>wl', '<C-w>l')
-utils.map('', '<leader>q', ':lua require \'tree\'.close()<CR> :wqa<CR>')
+
+
+utils.map('', '<leader>ff', ':Files<CR>')
 
 -- gereral bindings
 utils.map('', '<leader>tcc', ':set cursorcolumn!<CR>')
 utils.map('', '<leader>tcl', ':set cursorline!<CR>')
 
 -- explorer
-vim.api.nvim_set_keymap('n', '<leader>e', ':lua require \'tree\'.open()<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>E', ':lua require \'tree\'.close()<CR>', {noremap = true, silent = true})
-
--- dashboard
-vim.api.nvim_set_keymap('n', '<Leader>;', ':Dashboard<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<Leader>e', ':E<CR>', {noremap = true, silent = true})
 
 -- Comments
 vim.api.nvim_set_keymap("n", "<leader>/", ":CommentToggle<CR>", {noremap = true, silent = true})
 vim.api.nvim_set_keymap("v", "<leader>/", ":CommentToggle<CR>", {noremap = true, silent = true})
-
--- telescope
-utils.map('', '<leader>ht', ':Telescope colorscheme<CR>')
-utils.map('', '<leader>ff', ':Telescope find_files<CR>')
--- lua_utils.map('', '<leader>/', ':Telescope current_buffer_fuzzy_find<CR>')
-utils.map('', '<leader>sg', ':Telescope live_grep<CR>')
-utils.map('', '<leader>sf', ':Telescope find_files<CR>')
-utils.map('', '<leader>sc', ':Telescope colorscheme<CR>')
-utils.map('', '<leader>bs', ':Telescope buffers<CR>')
