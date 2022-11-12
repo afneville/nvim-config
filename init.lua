e8b7e1415bc657696451fafdd685084f8ce987f7
Alexander Neville <alexander.neville@icloud.com>
2022-11-12 21:20:21 +0000

    update init.lua

--- init.lua
+++ init.lua
@@ -1,3 +1,8 @@
-require("settings")
+require("settings") -- hello -- hello -- hello
 require("mappings")
 require("plugins")
+
+require("config.ts_config")
+require("config.nvim_tree_config")
+require("config.bufferline_config")
+require("config.indent_blankline_config")
