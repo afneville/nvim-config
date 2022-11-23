efaa35bea8cd9e95066c59fbec3cb053dd576d6d
Alexander Neville <git@afneville.com>
2022-11-23 20:27:44 +0000

    add plugins, new config and refactor

--- init.lua
+++ init.lua
@@ -1,8 +1,3 @@
-require("settings") -- hello -- hello -- hello
+require("settings")
 require("mappings")
 require("plugins")
-
-require("config.ts_config")
-require("config.nvim_tree_config")
-require("config.bufferline_config")
-require("config.indent_blankline_config")
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -13,18 +13,33 @@ return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
     use {
         'nvim-treesitter/nvim-treesitter',
-        run = ':TSUpdate'
+        run = ':TSUpdate',
+        config = require("config.treesitter")
     }
     use {
         'nvim-tree/nvim-tree.lua',
-        requires = { 'nvim-tree/nvim-web-devicons' }
+        requires = { 'nvim-tree/nvim-web-devicons' },
+        config = require("config.nvimtree")
+    }
+    use {
+        'junegunn/fzf.vim'
+    }
+    use {
+        "lukas-reineke/indent-blankline.nvim",
+        config = require("config.indentblankline")
+    }
+    use {
+        'akinsho/bufferline.nvim', 
+        requires = 'nvim-tree/nvim-web-devicons',
+        config = require("config.bufferline")
     }
     use {
       'nvim-lualine/lualine.nvim',
-      requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+      requires = { 'kyazdani42/nvim-web-devicons', opt = true },
+      config = require("config.lualine")
     }
-    use 'junegunn/fzf.vim'
-    use "lukas-reineke/indent-blankline.nvim"
-    use {'akinsho/bufferline.nvim', requires = 'nvim-tree/nvim-web-devicons'}
-
+    use {
+      'lewis6991/gitsigns.nvim',
+      config = require("config.gitsigns")
+    }
 end)
