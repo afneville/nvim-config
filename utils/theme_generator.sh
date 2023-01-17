449a6c209bfaffdf468bec0607be1aaaeb3ea80f
Alexander Neville <git@afneville.com>
2023-01-17 17:48:48 +0000

    generate themes as required (no longer version themes)

--- utils/theme_generator.sh
+++ utils/theme_generator.sh
@@ -1,17 +1,16 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 SCHEME_REPO_NAME="base16-schemes"
 SCHEME_REPO_HTTPS="https://github.com/tinted-theming/base16-schemes.git"
 WORKING_DIR="$(dirname $(realpath $0))"
 LOCAL_REPO="${WORKING_DIR}/${SCHEME_REPO_NAME}"
 TMP_DIR="${WORKING_DIR}/schemes"
-VIM_DIR="${WORKING_DIR}/colors" # i know, i know, colour is spelt wrong, but its a standard path for neovim
-LUA_DIR="${WORKING_DIR}/lua"
+OUT_DIR="${WORKING_DIR}/colors"
 
-get_schemes() {
-    echo $LOCAL_REPO
+get_remote_schemes() {
+    echo "$LOCAL_REPO"
     if [ ! -d "$LOCAL_REPO" ]; then
-        git clone $SCHEME_REPO_HTTPS & 
+        git clone "$SCHEME_REPO_HTTPS" &
     else
         git --git-dir=${LOCAL_REPO}/.git pull &
     fi
@@ -19,13 +18,11 @@ get_schemes() {
     wait
     echo "Done."
 }
-
-write_vim () {
+write_vim() {
     echo "let g:colors_name = 'b16-${2}'"
     printf "lua require('b16-theme').setup({"
-    for c in base0{0..9} base0{A..F}
-    do
-        printf " ${c} = '#`sed -ne 's/'"${c}"': "\(.*\)".*/\1/p' $1`'"
+    for c in base0{0..9} base0{A..F}; do
+        printf " ${c} = '#$(sed -ne 's/'"${c}"': "\(.*\)".*/\1/p' $1)'"
         [ ${c} != "base0F" ] && printf ","
     done
     printf " })\n"
@@ -33,21 +30,21 @@ write_vim () {
 
 process_themes() {
     echo "Writing themes ..."
-    for f in $TMP_DIR/*.yaml
-    do
+    for f in $LOCAL_REPO/*.yaml; do
         fname=$(echo $(basename $f) | cut -f 1 -d ".")
-        write_vim $f $fname > ${VIM_DIR}/b16-${fname}.vim &
+        colours=()
+        for c in base0{0..9} base0{A..F}; do
+            colours+=($(sed -ne 's/'"${c}"': "\(.*\)".*/\1/p' $f))
+        done
+        write_vim $f $fname >${OUT_DIR}/b16-${fname}.vim &
     done
     wait
     echo "Done."
 }
 
 main() {
-    mkdir -p $TMP_DIR
-    mkdir -p $VIM_DIR
-    get_schemes
-    cp $SCHEME_REPO_NAME/*.yaml $TMP_DIR
+    get_remote_schemes
+    mkdir -p $OUT_DIR
     process_themes
 }
-
 main
